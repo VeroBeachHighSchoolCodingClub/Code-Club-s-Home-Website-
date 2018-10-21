5beed186c55c482244ec63fc7ade43719d07651a
VBHS <vbhscode@gmail.com>
2018-10-21 07:37:06 -0400

    added controls

---
+++ controllers/controller.js
@@ -0,0 +1,20 @@
+module.exports = (app) => {
+
+
+  app.get('/', (req, res) => {
+    res.render('home', {location: 'Home'});
+  })
+
+  app.get('/about', (req, res) => {
+    res.render('about', {location: 'About Us'})
+  })
+
+  app.get('/projects', (req, res) => {
+    res.render('projects', {location: 'Our Projects'})
+  })
+
+  app.get('*', (req, res) => {
+    res.status(404).send("404 -- Sorry, we couldn't find your request.");
+  })
+
+}
