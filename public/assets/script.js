156ec44dd3c5755c39a3816374a2a37020738660
IkeHunter <ikehunter5@gmail.com>
2018-11-04 08:09:34 -0500

    started js integration with thumbnails

--- public/assets/script.js
+++ public/assets/script.js
@@ -1,1 +1,21 @@
 console.log('javascript connected');
+
+var pic1 = $('.pics1');
+var indPic = $('#ip1');
+var pContent = $('#p1Content');
+
+console.log(pic1, indPic);
+
+pic1.on('click', function() {
+  // pic.addClass('clearE');
+  pic1.css({
+    'padding': '10vw',
+
+  });
+  pContent.className = 'pContent';
+  indPic.css({
+    'width': '10vw',
+    // 'margin-top': '0.1vw'
+    'padding': '0'
+  })
+})
