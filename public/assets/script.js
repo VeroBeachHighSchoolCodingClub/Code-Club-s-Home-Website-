ec5b1abdccd5045c77a5cadde251dca53f83bba5
IkeHunter <ikehunter5@gmail.com>
2018-11-07 21:30:09 -0500

    added js on project thumbnail

--- public/assets/script.js
+++ public/assets/script.js
@@ -1,21 +1,37 @@
-console.log('javascript connected');
+$(document).ready(function(){
+  console.log('javascript connected');
 
-// var pic1 = $('.pics1');
-// var indPic = $('#ip1');
-// var pContent = $('#p1Content');
-//
-// console.log(pic1, indPic);
-//
-// pic1.on('click', function() {
-//   // pic.addClass('clearE');
-//   pic1.css({
-//     'padding': '10vw',
-//
-//   });
-//   pContent.className = 'pContent';
-//   indPic.css({
-//     'width': '10vw',
-//     // 'margin-top': '0.1vw'
-//     'padding': '0'
-//   })
-// })
+  var pic1 = $('#ePics1');
+  var indPic = $('#ip1');
+  var pContent = $('#p1Content');
+  var tp1 = 'tempPicStyle';
+  var cE = 'clearE';
+  var pCon = 'pContent';
+  var p1s = 'pics1'
+
+  // pic1.hover(function() {
+  //   pic1.removeClass(p1s);
+  //   pic1.addClass(tp1);
+  //   pContent.removeClass(cE);
+  //   pContent.addClass(pCon);
+  // }, function() {
+  //   pic1.removeClass(tp1);
+  //   pic1.addClass(p1s);
+  //   pContent.removeClass(pCon);
+  //   pContent.addClass(cE);
+  // });
+
+  pic1.on('mouseover click', function() {
+    pic1.removeClass(p1s);
+    pic1.addClass(tp1);
+    pContent.removeClass(cE);
+    pContent.addClass(pCon);
+  });
+  pic1.on('mouseout dblclick', function() {
+    pic1.removeClass(tp1);
+    pic1.addClass(p1s);
+    pContent.removeClass(pCon);
+    pContent.addClass(cE);
+  });
+
+});
