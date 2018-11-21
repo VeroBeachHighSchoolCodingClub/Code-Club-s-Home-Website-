4ed67a405a6ed43824ce262ffd29207cda93f5a9
Isaac <ikehunter5@gmail.com>
2018-11-21 15:44:48 -0500

    made project transition smoother

--- public/assets/script.js
+++ public/assets/script.js
@@ -1,105 +1,6 @@
 $(document).ready(function(){
   console.log('javascript connected');
 
-  // var pic1 = $('#ePics1');
-  // var pic2 = $('#ePics2');
-  // var pic3 = $('#ePics3');
-  // var pic4 = $('#ePics4');
-  // var indPic1 = $('#ip1');
-  // var indPic2 = $('#ip2');
-  // var indPic3 = $('#ip3');
-  // var indPic4 = $('#ip4');
-  // var p1Content = $('#p1Content');
-  // var p2Content = $('#p2Content');
-  // var p3Content = $('#p3Content');
-  // var p4Content = $('#p4Content');
-  // var tp1 = 'tps1';
-  // var tp2 = 'tps2';
-  // var cE = 'clearE';
-  // var p1Con = 'p1Content';
-  // var p2Con = 'p2Content';
-  // var p1s = 'pics1';
-  // var p2s = 'pics2';
-  // var nav = document.getElementById("navBar");
-  // var navStick = nav.offsetTop;
-
-//  function pic(num) {
-//     this.el = $('#ePics' + num);
-//     this.indPic = $('#ip' + num);
-//     this.pContent = $('#p' + num + 'Content');
-//     this.tp = 'tps' + num;
-//     this.pCon = 'p' + num + 'Content';
-//     this.ps = 'pics' + num;
-//  }
-
-  // pic1.on('mouseover click', function() {
-  //   pic1.removeClass(p1s);
-  //   pic1.addClass(tp1);
-  //   p1Content.removeClass(cE);
-  //   p1Content.addClass(p1Con);
-  //   indPic1.removeClass('ip1');
-  //   indPic1.addClass('tip1')
-  // });
-  // pic1.on('mouseout dblclick', function() {
-  //   pic1.removeClass(tp1);
-  //   pic1.addClass(p1s);
-  //   p1Content.removeClass(p1Con);
-  //   p1Content.addClass(cE);
-  //   indPic1.removeClass('tip1');
-  //   indPic1.addClass('ip1')
-  // });
-
-  // pic2.on('mouseover click', function() {
-  //   pic2.removeClass(p2s);
-  //   pic2.addClass(tp2);
-  //   p2Content.removeClass(cE);
-  //   p2Content.addClass(p2Con);
-  //   indPic2.removeClass('ip2');
-  //   indPic2.addClass('tip2')
-  // });
-  // pic2.on('mouseout dblclick', function() {
-  //   pic2.removeClass(tp2);
-  //   pic2.addClass(p2s);
-  //   p2Content.removeClass(p2Con);
-  //   p2Content.addClass(cE);
-  //   indPic2.removeClass('tip2');
-  //   indPic2.addClass('ip2')
-  // });
-
-  // pic3.on('mouseover click', function() {
-  //   pic3.removeClass(p1s);
-  //   pic3.addClass(tp1);
-  //   p3Content.removeClass(cE);
-  //   p3Content.addClass(p1Con);
-  //   indPic3.removeClass('ip1');
-  //   indPic3.addClass('tip1')
-  // });
-  // pic3.on('mouseout dblclick', function() {
-  //   pic3.removeClass(tp1);
-  //   pic3.addClass(p1s);
-  //   p3Content.removeClass(p1Con);
-  //   p3Content.addClass(cE);
-  //   indPic3.removeClass('tip1');
-  //   indPic3.addClass('ip1')
-  // });
-
-  // pic4.on('mouseover click', function() {
-  //   pic4.removeClass(p2s);
-  //   pic4.addClass(tp2);
-  //   p4Content.removeClass(cE);
-  //   p4Content.addClass(p2Con);
-  //   indPic4.removeClass('ip2');
-  //   indPic4.addClass('tip2')
-  // });
-  // pic4.on('mouseout dblclick', function() {
-  //   pic4.removeClass(tp2);
-  //   pic4.addClass(p2s);
-  //   p4Content.removeClass(p2Con);
-  //   p4Content.addClass(cE);
-  //   indPic4.removeClass('tip2');
-  //   indPic4.addClass('ip2')
-  // });
-
 class Pic1 {
   constructor(n) {
     this.el = $('#ePics' + n);
@@ -123,32 +24,38 @@ class Pic2 {
   }
 }
 
-
-
 var pic1 = new Pic1(1);
 
-pic1.el.on('mouseover click', function() {
-  pic1.el.removeClass(pic1.ps);
-  pic1.el.addClass(pic1.tp);
-  pic1.pContent.removeClass(pic1.cE);
-  pic1.pContent.addClass(pic1.pCon);
-  pic1.indPic.removeClass('ip1');
-  pic1.indPic.addClass('tip1');
-});
-pic1.el.on('mouseout dblclick', function() {
-  pic1.el.removeClass(pic1.tp);
-  pic1.el.addClass(pic1.ps);
-  pic1.pContent.removeClass(pic1.pCon);
-  pic1.pContent.addClass(pic1.cE);
-  pic1.indPic.removeClass('tip1');
-  pic1.indPic.addClass('ip1');
-});
+function over(){
 
+    pic1.el.on('mouseover click', function() {
+      setTimeout(function(){
+        pic1.el.removeClass(pic1.ps);
+        pic1.el.addClass(pic1.tp);
+        pic1.pContent.removeClass(pic1.cE);
+        pic1.pContent.addClass(pic1.pCon);
+        pic1.indPic.removeClass('ip1');
+        pic1.indPic.addClass('tip1');
+        console.log($("#ePics1").css("paddingTop"));
+      }, 350)
+    });
+}
 
+function out() {
+    pic1.el.on('mouseout dblclick', function() {
+      setTimeout(function(){
+        pic1.el.removeClass(pic1.tp);
+        pic1.el.addClass(pic1.ps);
+        pic1.pContent.removeClass(pic1.pCon);
+        pic1.pContent.addClass(pic1.cE);
+        pic1.indPic.removeClass('tip1');
+        pic1.indPic.addClass('ip1');
+      }, 350);
+    });
+}
 
-
-
-
+over();
+out();
 
   window.onscroll = function() {stickyNav()};
 
