3370f9143c2c6dda48c41b928dbaaa04fe0a9cfe
IkeHunter <ikehunter5@gmail.com>
2018-11-24 21:09:30 -0500

    fixed bug with mongo database in production

--- config/config.js
+++ config/config.js
@@ -2,8 +2,8 @@ var env = process.env.NODE_ENV || 'development';
 
 if (env === 'development') {
   process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://127.0.0.1:27017/VBHS';
+  process.env.MONGODB_URI = 'mongodb://127.0.0.1:27017/VBHS', { useNewUrlParser: true };
 } else if (env === 'test') {
   process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/VBHSTest';
+  process.env.MONGODB_URI = 'mongodb://localhost:27017/VBHSTest', { useNewUrlParser: true };
 }
--- db/mongoose.js
+++ db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://ikehunter5:Password1234@ds151382.mlab.com:51382/vbhs', { useNewUrlParser: true });
 
 mongoose.connection.once('open', function(){
     console.log('Database is connected!');
