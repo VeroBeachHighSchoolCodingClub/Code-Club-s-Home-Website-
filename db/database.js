a582a4527f35b99c11254c7084c76d5c593f4f7d
IkeHunter <ikehunter5@gmail.com>
2018-12-09 13:15:37 -0500

    bug fixing heroku crash

--- db/database.js
+++ db/database.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://admin:Crab5Overlord10@ds155150.mlab.com:55150/home-site', { useNewUrlParser: true });
 
-mongoose.connection.once('open', function(){
-    console.log('Database is connected!');
-}).on('error', function(error){
-    console.log('Connection Err: ', error);
-});
+// mongoose.connection.once('open', function(){
+//     console.log('Database is connected!');
+// }).on('error', function(error){
+//     console.log('Connection Err: ', error);
+// });
 
 module.exports = {mongoose};
