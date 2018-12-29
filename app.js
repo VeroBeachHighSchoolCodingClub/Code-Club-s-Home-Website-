12d15f310ea70ea1d48e1abe65369ce024a44b7f
Isaac <ikehunter5@gmail.com>
2018-12-29 15:02:24 -0500

    added if_eq hbs helper

--- app.js
+++ app.js
@@ -21,6 +21,14 @@ hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear()
 });
 
+hbs.registerHelper('if_eq', function(a, b, opts) {
+  if (a == b) {
+      return opts.fn(this);
+  } else {
+      return opts.inverse(this);
+  }
+});
+
 const port = process.env.PORT;
 
 app.use(express.static('./public'));
