1605bcf3743f7b8e5b2d53ea666a21eef8f4a0de
Isaac <ikehunter5@gmail.com>
2019-01-07 16:37:55 -0500

    members connected to db

--- db/models/members.js
+++ db/models/members.js
@@ -16,25 +16,62 @@ var Member = mongoose.model('member', {
     quote: {
         type: String,
         trim: false,
-        required: true,
+        required: false,
         maxlength: 200 
     },
     type: {
         type: String,
-        required: true,
+        required: false,
         maxlength: 10
     },
     career: {
         type: String,
         trim: false,
-        required: true,
+        required: false,
         maxlength: 30
     },
-    picture: {
-        type: String,
-        trim: true,
-        required: false
-    }
+    leader: {
+        type: Boolean,
+        required: true
+    },
+    picture: [{
+        fieldname: {
+            type: String,
+            required: true
+        },
+        originalname: {
+            type: String,
+            required: true
+        },
+        encoding: {
+            type: String,
+            required: true
+        },
+        mimetype: {
+            type: String,
+            required: true
+        },
+        destination: {
+            type: String,
+            required: true
+        },
+        filename: {
+            type: String,
+            required: true
+        },
+        path: {
+            type: String,
+            required: true
+        },
+        size: {
+            type: Number,
+            required: true
+        },
+        data: {
+            type: Buffer,
+            required: true
+        }
+    }]
 });
 
 module.exports = {Member};
